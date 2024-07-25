bbc6073af950321ae56157a3f00d09be37e976cd
Cristian <calvarez@trabajo.gob.hn>
2024-07-25 15:40:25 -0600

    Creacion de rutas, carpetas y la pagina de cafta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaftaComponent } from './pages/cafta/cafta.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { OrganizacionComponent } from './pages/organizacion/organizacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cafta', component: CaftaComponent},
+  {path: "contacto",component:ContactoComponent},
+  {path: "organizacion",component:OrganizacionComponent},
+
+  {
+    path: '**',
+    redirectTo: '/cafta',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { CaftaComponent } from './pages/cafta/cafta.component';
+import { OrganizacionComponent } from './pages/organizacion/organizacion.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CaftaComponent,
+    OrganizacionComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
